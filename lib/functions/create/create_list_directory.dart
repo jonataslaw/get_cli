dd2272f0894b62ea997359bb158e1940e217e87d
CpdnCristiano <Cristiano@proserv.net.br>
2020-08-25 20:55:42 -0300

    clean import

--- lib/functions/create/create_list_directory.dart
+++ lib/functions/create/create_list_directory.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:get_cli/core/structure.dart';
-
 Future<void> createListDirectory(List<Directory> dirs) async {
   dirs.forEach((element) async {
     await element.create();
