5fd49f2cb4e8f45f35835a91977f708bdc456667
CpdnCristiano <Cristiano@proserv.net.br>
2020-08-26 04:51:34 -0300

    desativar verificar se a update

--- bin/get.dart
+++ bin/get.dart
@@ -5,14 +5,15 @@ import 'package:get_cli/get_cli.dart';
 
 main(List<String> arguments) async {
   // acho melhor criar uma nova fuction
-  await PubDevApi.getLatestVersionFromPackage('get_cli')
+  // aumento de tempo pra realizar commandos precisa melhorar
+  /* await PubDevApi.getLatestVersionFromPackage('get_cli')
       .then((versionInPubDev) async {
     String versionInstalled = await PubspecLock.getVersionCli(disableLog: true);
     if (versionInstalled != versionInPubDev) {
       LogService.info('a new version is available');
       LogService.info('Run: get update');
     }
-  });
+  }); */
 
   Core core = Core();
   core.generate(arguments: List.from(arguments));
