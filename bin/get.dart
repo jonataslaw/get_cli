7cc8e0e368fec8810c999e5a746ed9971aa1ffec
CpdnCristiano <Cristiano@proserv.net.br>
2020-08-26 03:22:10 -0300

    remove comments on suggest update

--- bin/get.dart
+++ bin/get.dart
@@ -1,15 +1,18 @@
+import 'package:get_cli/common/utils/logger/LogUtils.dart';
+import 'package:get_cli/common/utils/pub_dev/pub_dev_api.dart';
+import 'package:get_cli/common/utils/pubspec/pubspec_lock.dart';
 import 'package:get_cli/get_cli.dart';
 
 main(List<String> arguments) async {
-  // não sei se é útil, desativado por enquanto
-  /*  await PubDevApi.getLatestVersionFromPackage('get_cli')
+  // acho melhor criar uma nova fuction
+  await PubDevApi.getLatestVersionFromPackage('get_cli')
       .then((versionInPubDev) async {
     String versionInstalled = await PubspecLock.getVersionCli(disableLog: true);
     if (versionInstalled != versionInPubDev) {
       LogService.info('a new version is available');
       LogService.info('Run: get update');
     }
-  }); */
+  });
 
   Core core = Core();
   core.generate(arguments: List.from(arguments));
