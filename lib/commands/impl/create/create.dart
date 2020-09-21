6bb40fb36c50de16589ffe25b7d4c297deb9af66
CpdnCristiano <Cristiano@proserv.net.br>
2020-09-21 16:55:40 -0300

    create

--- lib/commands/impl/create/create.dart
+++ lib/commands/impl/create/create.dart
@@ -3,8 +3,26 @@ import 'package:get_cli/core/generator.dart';
 mixin CreateMixin {
   final _args = GetCli.arguments;
 
-  String get onCommand =>
-      _args.length > 2 && _args[2] == 'on' ? _args[3] : null;
+  String get onCommand {
+    int onIndex = _args.indexWhere(
+      (element) => element == 'on',
+    );
+    return onIndex != -1 ? _args[onIndex + 1] : null;
+  }
+
+  String get withArgument {
+    int withIndex = _args.indexWhere(
+      (element) => element == 'with',
+    );
+    return withIndex != -1 ? _args[withIndex + 1] : null;
+  }
+
+  String get fromArgument {
+    int fromIndex = _args.indexWhere(
+      (element) => element == 'from',
+    );
+    return fromIndex != -1 ? _args[fromIndex + 1] : null;
+  }
 
   String get name {
     if (_args.first == 'init') {
