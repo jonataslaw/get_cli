d85ab8bbe66739e2e5ca8209abd6c99e253a53be
CpdnCristiano <Cristiano@proserv.net.br>
2020-10-09 15:41:18 -0300

    add install several packages

--- lib/commands/impl/install/install.dart
+++ lib/commands/impl/install/install.dart
@@ -1,6 +1,7 @@
 import 'package:get_cli/commands/interface/command.dart';
 import 'package:get_cli/common/utils/logger/LogUtils.dart';
 import 'package:get_cli/common/utils/pubspec/pubspec_utils.dart';
+import 'package:get_cli/common/utils/shell/shel.utils.dart';
 import 'package:get_cli/core/generator.dart';
 import 'package:get_cli/exception_handler/exceptions/cli_exception.dart';
 
@@ -10,15 +11,31 @@ class InstallCommand extends Command {
     List<String> args = List.from(GetCli.arguments);
     args.removeAt(0);
     var isDev = args.contains('--dev');
-    var packageInfo = args.first.split(':');
-    LogService.info('Installing package "${packageInfo.first}" …');
 
-    if (packageInfo.length == 1) {
-      await PubspecUtils.addDependencies(packageInfo.first, isDev: isDev);
+    if (args.length == 1) {
+      var packageInfo = args.first.split(':');
+      LogService.info('Installing package "${packageInfo.first}" …');
+      if (packageInfo.length == 1) {
+        await PubspecUtils.addDependencies(packageInfo.first,
+            isDev: isDev, runPubGet: false);
+      } else {
+        await PubspecUtils.addDependencies(packageInfo.first,
+            version: packageInfo[1], isDev: isDev, runPubGet: false);
+      }
     } else {
-      await PubspecUtils.addDependencies(packageInfo.first,
-          version: packageInfo[1], isDev: isDev);
+      for (var element in args) {
+        var packageInfo = element.split(':');
+        LogService.info('Installing package "${packageInfo.first}" …');
+        if (packageInfo.length == 1) {
+          await PubspecUtils.addDependencies(packageInfo.first,
+              isDev: isDev, runPubGet: false);
+        } else {
+          await PubspecUtils.addDependencies(packageInfo.first,
+              version: packageInfo[1], isDev: isDev, runPubGet: false);
+        }
+      }
     }
+    await ShellUtils.pubGet();
   }
 
   @override
