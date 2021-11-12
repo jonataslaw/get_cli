8f839f49290b23535636bd92eade9f1945409c79
Cristiano Nascimento <cristiano@proserv.net.br>
2021-11-12 06:21:05 -0300

    Better code to create new route

--- lib/functions/routes/get_add_route.dart
+++ lib/functions/routes/get_add_route.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:recase/recase.dart';
@@ -7,6 +6,7 @@ import '../../common/utils/logger/log_utils.dart';
 import '../../common/utils/pubspec/pubspec_utils.dart';
 import '../../core/internationalization.dart';
 import '../../core/locales.g.dart';
+import 'package:get_cli/extensions.dart';
 import '../../samples/impl/get_route.dart';
 import '../create/create_single_file.dart';
 import '../find_file/find_file_by_name.dart';
@@ -17,15 +17,16 @@ import 'get_support_children.dart';
 /// This command will create the route to the new page
 void addRoute(String nameRoute, String bindingDir, String viewDir) {
   var routesFile = findFileByName('app_routes.dart');
-  var lines = <String>[];
+  //var lines = <String>[];
+  var content = '';
 
   if (routesFile.path.isEmpty) {
-    RouteSample().create(skipFormatter: true);
+    RouteSample().create();
     routesFile = File(RouteSample().path);
-    lines = routesFile.readAsLinesSync();
+    content = routesFile.readAsStringSync();
   } else {
-    var content = formatterDartFile(routesFile.readAsStringSync());
-    lines = LineSplitter.split(content).toList();
+    content = formatterDartFile(routesFile.readAsStringSync());
+    //lines = LineSplitter.split(content).toList();
   }
   var pathSplit = viewDir.split('/');
 
@@ -45,36 +46,21 @@ void addRoute(String nameRoute, String bindingDir, String viewDir) {
   }
   var route = pathSplit.join('/');
 
-  var indexEndRoutes = lines.indexWhere((element) => element.startsWith('}'));
   var declareRoute = 'static const ${nameRoute.snakeCase.toUpperCase()} =';
   var line = "$declareRoute '/$route';";
-
   if (supportChildrenRoutes) {
     line = '$declareRoute ${_pathsToRoute(pathSplit)};';
-    var indexEndPaths =
-        lines.lastIndexWhere((element) => element.startsWith('}'));
-
     var linePath = "$declareRoute '/${pathSplit.last}';";
-    if (lines.indexWhere((element) => element.trim().startsWith(declareRoute),
-            indexEndRoutes) ==
-        -1) {
-      lines.insert(indexEndPaths, linePath);
-    }
+    content = content.appendClassContent('_Paths', linePath);
   }
+  content = content.appendClassContent('Routes', line);
 
-  if (lines.sublist(0, indexEndRoutes).indexWhere(
-            (element) => element.trim().startsWith(declareRoute),
-          ) ==
-      -1) {
-    lines.insert(indexEndRoutes, line);
-    addAppPage(nameRoute, bindingDir, viewDir);
-    LogService.success(
-        Translation(LocaleKeys.sucess_route_created).trArgs([nameRoute]));
-  } else {
-    LogService.error('${LocaleKeys.warning.tr}, route not created by conflict');
-  }
+  addAppPage(nameRoute, bindingDir, viewDir);
 
-  writeFile(routesFile.path, lines.join('\n'), overwrite: true, logger: false);
+  LogService.success(
+      Translation(LocaleKeys.sucess_route_created).trArgs([nameRoute]));
+
+  writeFile(routesFile.path, content, overwrite: true, logger: false);
 }
 
 /// Create routes from the path
--- lib/samples/impl/get_route.dart
+++ lib/samples/impl/get_route.dart
@@ -14,6 +14,7 @@ abstract class Routes{
 
 abstract class _Paths {
   
+  _Paths._();
 
 }
 
