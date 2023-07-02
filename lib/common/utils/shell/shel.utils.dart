87b5dbee23bd7fe4aeb2d1c173e693cda310aef8
padukadafa <padukadafa@gmail.com>
2023-07-02 22:07:14 +0700

    deleting dart migrate

--- lib/common/utils/shell/shel.utils.dart
+++ lib/common/utils/shell/shel.utils.dart
@@ -17,8 +17,6 @@ class ShellUtils {
 
   static Future<void> activatedNullSafe() async {
     await pubGet();
-    await run('dart migrate --apply-changes --skip-import-check',
-        verbose: true);
   }
 
   static Future<void> flutterCreate(
