7205c1db6963b3484a3de815e1e9f9c5ae7cf8d1
Cristiano Nascimeno <Cristiano@proserv.net.br>
2023-09-08 09:09:30 -0300

    refactor the get_cli

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -3,3 +3,4 @@ library get_cli.extensions;
 export 'package:get_cli/core/internationalization.dart' show TranslationExt;
 export 'package:get_cli/extensions/list.dart';
 export 'package:get_cli/extensions/string.dart';
+export 'package:get_cli/extensions/dart_code.dart';
